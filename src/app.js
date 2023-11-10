be5b08bd0175700b6728a871cf0454671f831a25
Prince Sharma <prince.sharma_cs20@gla.ac.in>
2023-11-11 02:26:49 +0530

    home page error fix

--- src/app.js
+++ src/app.js
@@ -1,11 +1,25 @@
 import express from "express";
 import routes from "./routes/index.js";
 import pageNotFound from "./utils/pageNotFound.js";
+import { PostModel } from "./models/index.js";
 
 const app = express();
 
 // middleware
 app.use(express.json());
+
+// Home page
+app.get("/", async (req, res) => {
+  try {
+    const posts = await PostModel.find();
+    res.status(200).json({
+      status: "success",
+      data: posts,
+    });
+  } catch (error) {
+    res.json(error);
+  }
+});
 
 // routes
 app.use("/api/v1", routes);
